42c5100681e0de3e66dfa68b2b1f95a81d76b8e5
Samuel <samuel.iza@outlook.com>
2025-10-23 13:44:14 -0600

    Netlify Portafolio script

--- minify.js
+++ minify.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+
+// Minificar index.html
 const html = fs.readFileSync('index.html', 'utf8');
 const minified = html
   .replace(/\s+/g, ' ')
@@ -6,3 +8,12 @@ const minified = html
   .replace(/<!--[\s\S]*?-->/g, '')
   .trim();
 fs.writeFileSync('index.html', minified);
+
+// Minificar portafolio.html
+const portfolioHtml = fs.readFileSync('portafolio.html', 'utf8');
+const portfolioMinified = portfolioHtml
+  .replace(/\s+/g, ' ')
+  .replace(/>\s+</g, '><')
+  .replace(/<!--[\s\S]*?-->/g, '')
+  .trim();
+fs.writeFileSync('portafolio.html', portfolioMinified);
